3591b6b2d515078b2289d9074caab39bbe69dda1
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-03-15 22:10:03 -0700

    commit

--- telegram_util/__init__.py
+++ telegram_util/__init__.py
@@ -17,7 +17,7 @@ def cutCaption(quote, suffix, limit):
 		result = quote + suffix
 	result = result.replace('https://', '')
 	result = result.replace('http://', '')
-	result = result.replace('_', '')
+	result = result.replace('_', '\_')
 	return result
 
 def isCN(title):
