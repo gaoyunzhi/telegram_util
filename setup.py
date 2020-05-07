24db64792270b0ed3c38f44882e6976bf5f24592
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-05-07 14:14:41 -0700

    commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="telegram_util",
-    version="0.0.78",
+    version="0.0.79",
     author="Yunzhi Gao",
     author_email="gaoyunzhi@gmail.com",
     description="Telegram Util.",
